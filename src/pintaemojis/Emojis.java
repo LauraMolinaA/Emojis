d4932287005ed6294682918594358e8ca6d37238
LauraMolinaA <157601803+LauraMolinaA@users.noreply.github.com>
2024-02-26 12:42:10 +0100

    Update Emojis.java

--- src/pintaemojis/Emojis.java
+++ src/pintaemojis/Emojis.java
@@ -6,6 +6,8 @@ public class Emojis {
 		// TODO Auto-generated method stub
 		for (int i=0; i<10; i++) {
 			System.out.printf("\uD83D\uDE00");
+			System.out.println("\uD83D\uDE02");
+			
 		}
 
 	}
